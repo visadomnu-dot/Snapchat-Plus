3053b01b71edea1f5ad96fbc0c26fa84fad39f2d
visadomnu-dot <visadomnu@gmail.com>
2025-10-21 00:56:54 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+  
+  //script of index.html file
+  
+  // Redirect to the main index page after a delay (e.g., 3 seconds)
+  setTimeout(function() {
+    window.location.href = "/Acconts.html"; // Redirect to your main index page
+}, 2400); // 3000ms = 3 seconds
+
+
+
+
+
+
